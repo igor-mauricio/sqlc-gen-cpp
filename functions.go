0427a6396a7da2cbbd93fe0ee9a2630a780d7672
Igor Maurício <igor.mauricio.eng@gmail.com>
2024-12-26 20:41:58 -0300

    refactor: extracted functions to its own file; feat: added support for a directory of templates

---
+++ functions.go
@@ -0,0 +1,81 @@
+package main
+
+import (
+	"bytes"
+	"log"
+	"os"
+	"path/filepath"
+	"regexp"
+	"strings"
+	"text/template"
+)
+
+func getFuncMap(schemaParam []string) template.FuncMap {
+	return template.FuncMap{
+		"add": func(a, b int) int {
+			return a + b
+		},
+		"upper": func(s string) string {
+			return strings.ToUpper(s)
+		},
+		"oneLineSQL": func(s string) string {
+			// Remove inline comments (e.g., "-- comment")
+			re := regexp.MustCompile(`--.*`)
+			withoutComments := re.ReplaceAllString(s, "")
+			// Replace newlines and extra spaces with a single space
+			cleaned := strings.ReplaceAll(withoutComments, "\n", " ")
+			cleaned = strings.Join(strings.Fields(cleaned), " ") // Removes extra spaces
+
+			return cleaned
+		},
+
+		"schema": func() string {
+			schema := ""
+			for _, schemaPath := range schemaParam {
+				fileInfo, err := os.Stat(schemaPath)
+				if err != nil {
+					log.Fatalf("Error accessing schema path: %v", err)
+				}
+				if !fileInfo.IsDir() {
+					content, err := readFile(schemaPath)
+					if err != nil {
+						log.Fatalf("Error reading file %s: %v", schemaPath, err)
+					}
+					schema += content + "\n"
+					continue
+				}
+				files, err := os.ReadDir(schemaPath)
+				if err != nil {
+					log.Fatalf("Error reading directory: %v", err)
+				}
+				for _, file := range files {
+					if file.IsDir() { // Skip subdirectories
+						continue
+					}
+					filePath := filepath.Join(schemaPath, file.Name())
+					content, err := readFile(filePath)
+					if err != nil {
+						log.Fatalf("Error reading file %s: %v", filePath, err)
+					}
+					schema += content + "\n"
+				}
+			}
+			return schema
+		},
+	}
+}
+
+func readFile(path string) (string, error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return "", err
+	}
+	defer file.Close()
+
+	buf := new(bytes.Buffer)
+	_, err = buf.ReadFrom(file)
+	if err != nil {
+		return "", err
+	}
+	return buf.String(), nil
+}
--- main.go
+++ main.go
@@ -8,8 +8,6 @@ import (
 	"log"
 	"os"
 	"os/exec"
-	"path/filepath"
-	"regexp"
 	"strings"
 	"text/template"
 
@@ -23,7 +21,6 @@ func main() {
 
 type Options struct {
 	Template         string `json:"template" yaml:"template"`
-	Filename         string `json:"filename" yaml:"filename"`
 	FormatterCommand string `json:"formatter_cmd" yaml:"formatter_cmd"`
 	Out              string `json:"out" yaml:"out"`
 }
@@ -41,107 +38,75 @@ func parseOpts(req *plugin.GenerateRequest) (*Options, error) {
 }
 
 func generate(ctx context.Context, req *plugin.GenerateRequest) (*plugin.GenerateResponse, error) {
-	options, _ := parseOpts(req)
-
-	templateFileName := options.Template
-
-	funcMap := template.FuncMap{
-		"add": func(a, b int) int {
-			return a + b
-		},
-		"upper": func(s string) string {
-			return strings.ToUpper(s)
-		},
-		"oneLineSQL": func(s string) string {
-			// Remove inline comments (e.g., "-- comment")
-			re := regexp.MustCompile(`--.*`)
-			withoutComments := re.ReplaceAllString(s, "")
-			// Replace newlines and extra spaces with a single space
-			cleaned := strings.ReplaceAll(withoutComments, "\n", " ")
-			cleaned = strings.Join(strings.Fields(cleaned), " ") // Removes extra spaces
-
-			return cleaned
-		},
-
-		"schema": func() string {
-			schema := ""
-			for _, schemaPath := range req.Settings.Schema {
-				fileInfo, err := os.Stat(schemaPath)
-				if err != nil {
-					log.Fatalf("Error accessing schema path: %v", err)
-				}
-				if !fileInfo.IsDir() {
-					content, err := readFile(schemaPath)
-					if err != nil {
-						log.Fatalf("Error reading file %s: %v", schemaPath, err)
-					}
-					schema += content + "\n"
-					continue
-				}
-				files, err := os.ReadDir(schemaPath)
-				if err != nil {
-					log.Fatalf("Error reading directory: %v", err)
-				}
-				for _, file := range files {
-					if file.IsDir() { // Skip subdirectories
-						continue
-					}
-					filePath := filepath.Join(schemaPath, file.Name())
-					content, err := readFile(filePath)
-					if err != nil {
-						log.Fatalf("Error reading file %s: %v", filePath, err)
-					}
-					schema += content + "\n"
-				}
-			}
-			return schema
-		},
-	}
-
-	tmpl, err := template.New(templateFileName).Funcs(funcMap).ParseFiles(templateFileName)
+	options, err := parseOpts(req)
 	if err != nil {
-		log.Fatalf("Error parsing template file: %v", err)
+		return nil, fmt.Errorf("parsing plugin options: %w", err)
 	}
+	templatePathName := options.Template
 
 	resp := plugin.GenerateResponse{}
 
-	var buf bytes.Buffer
-	err = tmpl.Execute(&buf, req)
+	//check if the path is a directory
+
+	fileInfo, err := os.Stat(templatePathName)
 	if err != nil {
-		log.Fatalf("Error executing template: %v", err)
+		log.Fatalf("Error accessing template path: %v", err)
 	}
 
-	if options.FormatterCommand != "" {
-		execCommand := exec.Command("/usr/bin/env", "bash", "-c", options.FormatterCommand)
-		execCommand.Stdin = bytes.NewReader(buf.Bytes())
-		var output bytes.Buffer
-		execCommand.Stdout = &output
-		if err := execCommand.Run(); err != nil {
-			log.Fatalf("Error executing formatter command: %v", err)
+	if !fileInfo.IsDir() {
+		tmpl, err := template.New(templatePathName).Funcs(getFuncMap(req.Settings.Schema)).ParseFiles(templatePathName)
+		if err != nil {
+			log.Fatalf("Error parsing template file: %v", err)
 		}
-
-		buf = output
+		var buf bytes.Buffer
+		err = tmpl.Execute(&buf, req)
+		if err != nil {
+			log.Fatalf("Error executing template: %v", err)
+		}
+		// runFormatter(&buf, options.FormatterCommand)
+		resp.Files = append(resp.Files, &plugin.File{
+			Name:     strings.TrimSuffix(templatePathName, ".tmpl"),
+			Contents: buf.Bytes(),
+		})
+		return &resp, nil
 	}
-
-	resp.Files = append(resp.Files, &plugin.File{
-		Name:     options.Filename,
-		Contents: buf.Bytes(),
-	})
+	files, err := os.ReadDir(templatePathName)
+	if err != nil {
+		log.Fatalf("Error reading directory: %v", err)
+	}
+	for _, file := range files {
+		relativePath := fmt.Sprintf("%s/%s", templatePathName, file.Name())
+		fileName := file.Name()
+		if file.IsDir() {
+			continue
+		}
+		tmpl, err := template.New(fileName).Funcs(getFuncMap(req.Settings.Schema)).ParseFiles(relativePath)
+		if err != nil {
+			log.Fatalf("Error parsing template file: %v", err)
+		}
+		var buf bytes.Buffer
+		err = tmpl.Execute(&buf, req)
+		if err != nil {
+			log.Fatalf("Error executing template: %v", err)
+		}
+		// runFormatter(&buf, options.FormatterCommand)
+		resp.Files = append(resp.Files, &plugin.File{
+			Name:     strings.TrimSuffix(fileName, ".tmpl"),
+			Contents: buf.Bytes(),
+		})
+	}
 
 	return &resp, nil
 }
 
-func readFile(path string) (string, error) {
-	file, err := os.Open(path)
-	if err != nil {
-		return "", err
+func runFormatter(buffer *bytes.Buffer, formatterCommand string) {
+	execCommand := exec.Command("/usr/bin/env", "bash", "-c", formatterCommand)
+	execCommand.Stdin = bytes.NewReader(buffer.Bytes())
+	var output bytes.Buffer
+	execCommand.Stdout = &output
+	if err := execCommand.Run(); err != nil {
+		log.Fatalf("Error executing formatter command: %v", err)
 	}
-	defer file.Close()
 
-	buf := new(bytes.Buffer)
-	_, err = buf.ReadFrom(file)
-	if err != nil {
-		return "", err
-	}
-	return buf.String(), nil
+	*buffer = output
 }
